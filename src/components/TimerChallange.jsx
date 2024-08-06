cafe5dccac0c2b061a157c3634f5635a6a65eeb7
sevakul16 <sevakul16@gmail.com>
2024-08-06 16:54:11 +0300

    Added logic to timers and a modal

--- src/components/TimerChallange.jsx
+++ src/components/TimerChallange.jsx
@@ -1,17 +1,44 @@
 import React from "react";
+import { useState, useRef } from "react";
+import ResultModal from "./ResultModal";
 
 const TimerChallange = ({ title, targetTime }) => {
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  const timer = useRef();
+  const dialog = useRef();
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      dialog.current.open();
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
   return (
-    <section className="challenge">
-      <h2>{title}</h2>
-      <p className="challenge-time">
-        {targetTime} second{targetTime > 1 ? "s" : ""}
-      </p>
-      <p>
-        <button>Start</button>
-      </p>
-      <p className="active">Time is running/Time is not running</p>
-    </section>
+    <>
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <section className="challenge">
+        <h2>{title}</h2>
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? "Stop" : "Start"}
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : undefined}>
+          {timerStarted ? "Timer is running" : "Timer is stopped"}
+        </p>
+      </section>
+    </>
   );
 };
 
